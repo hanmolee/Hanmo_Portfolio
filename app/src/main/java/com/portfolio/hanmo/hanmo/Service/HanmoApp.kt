7fd5a95f3364810f4394cc6156d9e47b0b6e7648
hanmo <dlgksah@gmail.com>
2018-02-03 15:54:09 +0900

    first commit

---
+++ app/src/main/java/com/portfolio/hanmo/hanmo/Service/HanmoApp.kt
@@ -0,0 +1,13 @@
+package com.portfolio.hanmo.hanmo.Service
+
+import android.app.Application
+import io.realm.Realm
+
+/**
+ * Created by hanmo on 2018. 2. 1..
+ */
+class HanmoApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
